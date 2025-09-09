80991482847860b88d966bf4f132f576856fbf8d
Vinod <vinodkv1990@gmail.com>
2025-09-09 07:10:21 +0530

    updates

--- src/index.ts
+++ src/index.ts
@@ -4,44 +4,47 @@ export default {
   register({ strapi }: { strapi: Core.Strapi }) {
     strapi.db.lifecycles.subscribe(async (event) => {
       const { model, action, result } = event;
+
       if(event.action === 'afterUpdate') {
 
         const ct = strapi.contentType(model.uid as any);
         
         if (model.uid === 'api::url-mapper.url-mapper') return;
 
-        // if(!ct) return;
+        if(!ct) return;
 
-        // if(!ct.attributes.title && !ct.attributes.name) return;
+        if(!result.title) return;
 
-        // if(!result.title && !result.name) return;
+        if(!result.url) return;
 
-        // if(!result.slug) return;
+        const isPublished = result?.createdBy?.publishedAt;
 
-        // if(ct?.kind === 'singleType') {
-        //   const title = result.title;
-        //   const url = result.slug;
-        //   strapi.entityService.create('api::url-mapper.url-mapper', {
-        //     data: {
-        //       title,
-        //       url,
-        //       type: 'single',
-        //       cid: result.id,
-        //       uid: model.uid,
-        //     },
-        //   });
-        // }
+        console.log('result==', result)
+
+        if(!isPublished) return;
+
+        if(ct?.kind === 'singleType') {
+          const title = result.title;
+          const url = result.slug;
+          strapi.entityService.create('api::url-mapper.url-mapper', {
+            data: {
+              title,
+              url,
+              type: 'single',
+              cid: result.id,
+              uid: model.uid,
+            },
+          });
+        }
                 
         if(ct?.kind === 'collectionType') {
           const title = result?.title;
           const url = result?.url;
-          console.log('urls==', url)
           const existing = await strapi.db.query('api::url-mapper.url-mapper').findOne({
             where: {
               url,
             },
           })
-          console.log('existing==', existing)
           if(existing) return;
           strapi.entityService.create('api::url-mapper.url-mapper', {
             data: {
